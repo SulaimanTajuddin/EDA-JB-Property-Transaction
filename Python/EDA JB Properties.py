9e80960d5efcca507350f9c6419ac6e6f13eccbe
SulaimanTajuddin <sulaimantajuddin96@gmail.com>
2025-06-01 21:05:24 +0800

    Updated 1/6/2025
    Updating EDA JB Property Transaction

---
+++ Python/EDA JB Properties.py
@@ -0,0 +1,53 @@
+import chardet # for automatically detect the character encoding of files
+import pandas as pd # for data manipulation and analysis
+import matplotlib.pyplot as plt
+
+# Detect the encoding
+with open('JB Properties Data 2021-2024 (TSV).csv', 'rb') as f: # rb: read binary mode
+    result = chardet.detect(f.read()) # analyzes the raw bytes to guess the character encoding
+    print(f"Detected encoding: {result['encoding']}") # print the encoding result
+
+# Load TSV with detected encoding
+df = pd.read_csv('JB Properties Data 2021-2024 (TSV).csv', sep='\t', encoding=result['encoding'])
+
+#---------------------------------------------------------------------------------------------------------------------
+'''
+# Overview of raw data + Data inspection:
+
+print(df.head(5))                      # print output of the first n row
+print(df.dtypes)                       # print output of the data types by column
+print("\n(Total no. of rows, Total no. of columns) = " + f"{df.shape}")
+print("\nNull values (True/False):\n") 
+print(df.isnull())                     # Check for null values in all cell, returns print output True/False for each cell
+print("\nTotal sum of duplicated values = " + f"{df.duplicated().sum()}\n")
+'''
+#---------------------------------------------------------------------------------------------------------------------
+# Data cleansing and filtering starts here:
+
+# Step 1: Clean column names
+df.columns = df.columns.str.strip() # The strip() method removes any leading and trailing whitespace from a string.
+
+# Step 2: Extract transaction year (filtering)
+df['Transaction Year'] = df['Month, Year of Transaction Date'].str.extract(r'(\d{4})').astype(int)
+
+# Step 3:Clean and convert 'Transaction Price' to numeric 
+df['Transaction Price'] = df['Transaction Price'].str.replace('RM', '', regex=False)
+df['Transaction Price'] = df['Transaction Price'].str.replace(',', '', regex=False).astype(float)
+
+# Step 4: Filter data for the years 2021 to 2024
+filtered_df = df[df['Transaction Year'].isin([2021, 2022, 2023, 2024])]
+
+#----------------------------------------------------------------------------------------------------------------------
+# Analysis starts here:
+
+# Step 1: Calculate median and standard deviation by year
+summary_by_year = filtered_df.groupby('Transaction Year')['Transaction Price'].agg(
+    Median ='median',Std_Dev='std',Mean='mean',Min='min',Max='max'
+    ).reset_index()
+
+# Step 2: Display result
+print(summary_by_year)
+
+# Step 3: Visualization
+
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# EDA-JB-Property-Transaction
+# Exploratory data analysis for JB properties transaction
+
+1) The data is taken from NAPIC website: https://napic2.jpph.gov.my/ms/data-transaksi?category=36&id=241
+2) Utilizing both Microsoft Power BI and Python for comparison
+ 
